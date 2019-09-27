e403788b623ea3f56ca533f1a92f0b4131f12121
Kai R <kairichardson2000@gmail.com>
2019-09-27 11:43:14 -0700

    adding loop work

---
+++ LoopExamples/LoopExample.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class LoopExample here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LoopExample
+{
+    int sum1toN(int n)
+    {
+    int sum = 0;
+    for (int i = 1; i <= n; i += 1)
+    {
+        sum += i;
+    }
+    return sum;
+  }
+}
