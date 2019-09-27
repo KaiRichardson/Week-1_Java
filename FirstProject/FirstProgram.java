3ce5796cdefd3bacacffbed646abc1851d4cd73b
Kai R <kairichardson2000@gmail.com>
2019-09-27 11:41:01 -0700

    adding loop study

---
+++ FirstProject/FirstProgram.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class FirstProgram here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FirstProgram
+{
+    public static void main(String[] a)
+    {
+        System.out.println("Hi World!");
+    }
+}
